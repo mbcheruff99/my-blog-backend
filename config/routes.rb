538131ed1b4e47780280a7ef822eee94fa9222d3
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-12-02 20:02:08 -0500

    blog backend, added user model, controller, and passwords/email stuf

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+
+  get "/posts" => "posts#index"
+  get "/posts/:id" => "posts#show"
+  post "/posts" => "posts#create"
+  patch "/posts/:id" => "posts#update"
+  delete "/posts/:id" => "posts#destroy"
+  
+  post "/users" => "users#create"
+end
