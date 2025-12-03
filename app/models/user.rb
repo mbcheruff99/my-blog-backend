8baf0f0e92715e87b13cc7bb6bf27b1c075bf6c4
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-12-02 20:09:17 -0500

    added user password needs 6 charachters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :email, presence: true, uniqueness: true 
+  validates :password, presence: true
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
 end
